4c8baa8b8d033500aec9ba645ac19217a95e4ebf
Daniel Siutkowski <dan.siutkowski@gmail.com>
2022-05-02 15:31:35 +0200

    working chatbot with limited responses

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from curses import ERR
+from pprint import pprint
 import random
 import json
 import pickle
 import numpy as np
 
 import nltk
+nltk.data.path.append('./nltk_data/')
 from nltk.stem import WordNetLemmatizer
 
 from tensorflow.python.keras.models import load_model
@@ -12,3 +15,50 @@ lemmatizer = WordNetLemmatizer()
 
 with open ("intents.json", 'r', encoding='utf8') as f:
     intents = json.load(f)
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.model')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+print("Go! Bot is running!")
+
+while True:
+    message = input("")
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print(res)
