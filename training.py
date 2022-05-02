b692824767f9b5e18438316610764fd4576e19bb
Daniel Siutkowski <dan.siutkowski@gmail.com>
2022-05-02 21:38:53 +0200

    added new entries to intents.json file

--- training.py
+++ training.py
@@ -69,6 +69,6 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = gradient_descent_v2.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbotmodel.h5', hist)
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
 print("Done")
